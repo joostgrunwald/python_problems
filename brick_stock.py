858fe43a08e147d195c1d2840ec053f548140b44
Joost Grunwald <joostgrunwald2001@gmail.com>
2022-02-09 15:05:22 +0100

    initial commit

---
+++ brick_stock.py
@@ -0,0 +1,16 @@
+"""
+Lets say we are hired by a brick company that sells bricks to people.
+The brick company has a single factory that makes certain kind of bricks.
+https://capricebaksteen.nl
+Now lets say for a given month, an x amount of bricks are produced every week.
+The brick factory wants to be able to see how many bricks of a certain type are in stock.
+They also want to see how many they are still expected to produce.
+In addition to this they want to have a model that shows possible problems with in and output.
+During the course of a few lessons we are going to help this brick company achieve this.
+We will start each lesson with a bit of theory, and then follow by solving the problems one by one. 
+
+Today we are first going to use functions, loops and global variables in order to manage stock.
+Exercise 1a: create some variable to save the amount of stock for the following bricks: "DCR, DCER, WSEMA-B"
+Exercise 1b: the first week the amount of produces bricks for WSEMA-B was 1234, the second week 987, the third week 1204, save these in a list.
+"""
+
